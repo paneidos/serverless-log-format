55c5747115220d2b3a0d1a32d77fbb07fa977d77
Sernin van de Krol <serninpc@paneidos.net>
2024-04-06 21:09:09 +0200

    Extend configuration schema for logging

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,15 @@ class LogFormatPlugin implements Plugin {
     constructor(serverless: Serverless, options: Serverless.Options, { log }: Plugin.Logging) {
         this.serverless = serverless
         this.hooks = {}
+
+        // For reference on JSON schema, see https://github.com/ajv-validator/ajv
+        serverless.configSchemaHandler.defineFunctionProperties('aws', {
+            properties: {
+                logFormat: { type: 'string', enum: ['Text', 'JSON'] },
+                logLevel: { type: 'string', enum: ['TRACE', 'DEBUG', 'INFO', 'WARN', 'ERROR', 'FATAL'] },
+                systemLogLeve: { type: 'string', enum: ['DEBUG', 'INFO', 'WARN'] },
+            },
+        });
     }
 }
 
