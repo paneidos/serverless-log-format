eca0824b357e537c8338136c4b7c59d826fd63fb
Sernin van de Krol <serninpc@paneidos.net>
2024-04-07 14:02:45 +0200

    Assign log config for individual functions

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,29 @@
 import Serverless from "serverless";
-import Plugin from "serverless/classes/Plugin";
+import type Plugin from "serverless/classes/Plugin";
+import type Aws from "serverless/plugins/aws/provider/awsProvider";
+
+type FunctionLogging = {
+    logFormat?: 'Text' | 'JSON'
+    logLevel?: 'TRACE' | 'DEBUG' | 'INFO' | 'WARN' | 'ERROR' | 'FATAL'
+    systemLogLevel?: 'DEBUG' | 'INFO' | 'WARN'
+}
 
 class LogFormatPlugin implements Plugin {
     commands: Plugin.Commands | undefined;
     hooks: Plugin.Hooks;
     serverless: Serverless
+    provider: Aws
+    log: Plugin.Logging['log']
 
     constructor(serverless: Serverless, options: Serverless.Options, { log }: Plugin.Logging) {
         this.serverless = serverless
-        this.hooks = {}
+        this.provider = serverless.getProvider('aws')
+        this.log = log
+        this.hooks = {
+            'after:package:compileFunctions': async() => {
+                await this.assignLogConfigs()
+            }
+        }
 
         // For reference on JSON schema, see https://github.com/ajv-validator/ajv
         serverless.configSchemaHandler.defineFunctionProperties('aws', {
@@ -18,6 +33,32 @@ class LogFormatPlugin implements Plugin {
                 systemLogLeve: { type: 'string', enum: ['DEBUG', 'INFO', 'WARN'] },
             },
         });
+    }
+
+    setFunctionLogConfig(logicalId: string, property: string, value: string) {
+        this.serverless.service.provider.compiledCloudFormationTemplate.Resources[logicalId].Properties.LoggingConfig = {
+            ...(this.serverless.service.provider.compiledCloudFormationTemplate.Resources[logicalId].Properties.LoggingConfig ?? {}),
+            [property]: value
+        }
+    }
+
+    async assignLogConfigs() {
+        Object.entries(this.serverless.service.functions).forEach(([name, config]) => {
+            const logConfig = Object.assign({}, config) as FunctionLogging
+            const logicalId = this.provider.naming.getLambdaLogicalId(name)
+
+            this.log.warning(this.provider.naming.getLambdaLogicalId(name));
+
+            if (logConfig.logFormat !== undefined) {
+                this.setFunctionLogConfig(logicalId, 'LogFormat', logConfig.logFormat)
+            }
+            if (logConfig.logLevel !== undefined) {
+                this.setFunctionLogConfig(logicalId, 'ApplicationLogLevel', logConfig.logLevel)
+            }
+            if (logConfig.systemLogLevel !== undefined) {
+                this.setFunctionLogConfig(logicalId, 'SystemLogLevel', logConfig.systemLogLevel)
+            }
+        })
     }
 }
 
